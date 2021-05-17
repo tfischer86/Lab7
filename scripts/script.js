64124592e109b71bb981d281d2c68de0884b4516
Thomas Fischer <tfischer@ucsd.edu>
2021-05-16 20:55:42 -0700

    Change paths for gh pages

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ const setState = router.setState;
 // Make sure you register your service worker here too
 if ("serviceWorker" in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register("./sw.js");
+    navigator.serviceWorker.register("sw.js");
   });
 }
 
